c787850ac858ce1cd2d11bce3c57bd7c16a7ab00
sahil shaikh <143120964+sahilshaikhss@users.noreply.github.com>
2025-02-12 23:20:04 +0530

    h

---
+++ .vscode/Strings.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+/*char arr1[100] ="Hello1";
+char arr2[100] ="hello4";
+char arr3[100] ="new one";
+
+cout<<strlen(arr1) <<endl;
+cout<<strcmp(arr1, arr1) <<endl;
+cout<<arr3<<endl;
+
+for (char ch :arr1)
+{
+    cout<<(int)ch<<endl;
+    if ((int)ch ==0)
+    {break;}
+
+*/
+string s;
+getline(cin , s);
+}
+
+
+
+
+
+
+
+
+
+
