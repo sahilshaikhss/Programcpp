71ec2bd9c8859a8be2b07a84756bb93e06b882ec
sahil shaikh <143120964+sahilshaikhss@users.noreply.github.com>
2025-02-12 22:56:10 +0530

    h

---
+++ .vscode/referencevariable.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void modify(int a)
+{
+
+    a++;
+
+}
+void refmodify(int &a)
+{
+    a++;
+
+}
+int main()
+{
+int a=10;
+int b=a;
+int &ref =a;
+a= a+1;
+cout<< "a:"<<a<<endl;
+cout<< "b:"<<b<<endl;
+cout<< "ref:"<<ref<<endl;
+a=11;
+b=a;
+ref = ref +1;
+cout<< "a:"<<a<<endl;
+cout<< "b:"<<b<<endl;
+cout<< "ref:"<<ref<<endl;
+
+b=10;
+cout<<"before:"<<b<<endl;
+modify(b);
+cout<<"After:"<<b<<endl;
+
+b=10;
+cout<<"before:"<<b<<endl;
+refmodify(b);
+cout<<"After:"<<b<<endl;
+/*
+in reference variable the value of the ref variable is assign to the its equal
+one and if any chnages in the ref or the one varibale is reflect to bothone. 
+*/
+
+}
